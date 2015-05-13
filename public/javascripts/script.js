8fd0971786656910d72c8b5fe9ab16ae1b08e215
Joseph Szczesniak <nukapunk@gmail.com>
2015-05-13 18:35:03 -0500

    Finished pro-mode

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
     container = $('.js-assignments');
+    editPanel = $('.js-editPanel');
+    nameEditor = $('#name');
+    scoreEditor = $('#score');
+    dateEditor = $('#date_completed');
+    editorSubmit = $('#submit');
 
     getData();
-
     assignClicks();
 });
 
@@ -13,6 +17,11 @@ var monthNames = [
     "August", "September", "October",
     "November", "December"
 ];
+var nameEditor;
+var scoreEditor;
+var dateEditor;
+var editPanel;
+var editorSubmit;
 
 function getData(){
     $.ajax({
@@ -21,15 +30,14 @@ function getData(){
         method: 'get',
         dataType: 'json',
         success: function(data, textStatus, jqXHR){
-            console.log(data);
             clearData();
-            appendData(data);
+            processData(data);
         },
         error: function(jqXHR, textStatus, errorThrown){
             console.log(textStatus,errorThrown);
         },
         complete: function(jqXHR, textStatus){
-            console.log("Ajax Get Complete:", textStatus);
+            console.log("getData() Ajax Get Complete:", textStatus);
         }
     });
 }
@@ -42,13 +50,37 @@ function deleteData(id){
         method: 'delete',
         dataType: 'json',
         success: function(data, textStatus, jqXHR){
+            //hide and remove
+            $('#' + id).slideUp(function() {
+                $(this).remove();
+            });
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+            console.log(textStatus,errorThrown);
+        },
+        complete: function(jqXHR, textStatus){
+            console.log("deleteData() Ajax Get Complete:", textStatus);
+        }
+    });
+}
+
+function updateData(data){
+    $.ajax({
+        url: '/assignments/' + data.id,
+        data: data,
+        method: 'put',
+        dataType: 'json',
+        success: function(data, textStatus, jqXHR){
+            // clear form
+            clearEditor();
+            // get new data and update
             getData();
         },
         error: function(jqXHR, textStatus, errorThrown){
             console.log(textStatus,errorThrown);
         },
         complete: function(jqXHR, textStatus){
-            console.log("Ajax Get Complete:", textStatus);
+            console.log("updateData() Ajax Get Complete:", textStatus);
         }
     });
 }
@@ -58,52 +90,119 @@ function clearData(){
     container.empty();
 }
 
-function appendData(assignments){
+function processData(assignments){
 
     for(var i = 0; i< assignments.length; i++){
         var assignment = assignments[i];
 
+        var id = assignment._id;
         var name = assignment.name || '';
 
-        var date = new Date(assignment.date_completed);
-        var day = date.getDate() || '';
-        var monthIndex = date.getMonth() || -1;
-        var year = date.getFullYear() || '';
-        var month = monthIndex == -1 ? '' : monthNames[monthIndex];
+        var year = assignment.date_completed.substring(0,4);
+        var monthIndex = assignment.date_completed.substring(5,7);
+        var day = assignment.date_completed.substring(8,10);
+
+        var month = monthNames[parseInt(monthIndex) -1];
 
         var score = assignment.score || 0;
 
-        var section = $('<section/>');
+        // needs to stay in format yyyy-MM-dd for date pickers
+        var datePicker = assignment.date_completed.substring(0,10);
 
-        var ul = $('<ul/>')
-            .appendTo(section);
-
-        var liName = $('<li/>')
-            .text('Name: ' + name)
-            .appendTo(ul);
-
-
-        var liScore = $('<li/>')
-            .text('Score: ' + score)
-            .appendTo(ul);
-
-
-        var liDate = $('<li/>')
-            .text('Date Completed: '+ day + ' ' + month + ' ' + year)
-            .appendTo(ul);
-
-        var btn = $('<button/>')
-            .text('delete')
-            .attr('id', assignment._id)
-            .appendTo(ul);
-
-        container.append(section);
+        buildAndAppendData(id, name, score, datePicker, day, month, year);
     }
 }
 
+function buildAndAppendData(id, name, score, datePicker, day, month, year){
+    var section = $('<section/>')
+        .attr('id', id)
+        .attr('data-name', name)
+        .attr('data-score', score)
+        .attr('data-date', datePicker);
+
+    var ul = $('<ul/>')
+        .appendTo(section);
+
+    var liName = $('<li/>')
+        .text('Name: ' + name)
+        .addClass('js-name')
+        .appendTo(ul);
+
+
+    var liScore = $('<li/>')
+        .text('Score: ' + score)
+        .addClass('js-score')
+        .appendTo(ul);
+
+
+    var liDate = $('<li/>')
+        .text('Date Completed: '+ month + ' ' + day + ', ' + year)
+        .addClass('js-date')
+        .appendTo(ul);
+
+
+    var liDelete = $('<li/>')
+        .appendTo(ul);
+
+    var btnDelete = $('<button/>')
+        .text('delete')
+        .attr('data-id', id)
+        .addClass('js-delete')
+        .appendTo(liDelete);
+
+    var liEdit = $('<li/>')
+        .appendTo(ul);
+
+    var btnEdit = $('<button/>')
+        .text('edit')
+        .addClass('js-edit')
+        .attr('data-id', id)
+        .appendTo(liEdit);
+
+    container.append(section);
+}
+
 function assignClicks(){
-    container.on('click', 'button', function(){
-        var id = $(this).attr('id');
+    container.on('click', '.js-delete', function(){
+        var id = $(this).data('id');
         deleteData(id);
-    })
+    });
+
+    container.on('click', '.js-edit', function(){
+        var id = $(this).data('id');
+        var section = $('#' + id);
+        var name = section.data('name');
+        var score = section.data('score');
+        var date = section.data('date');
+        showEditor(id, name, score, date);
+    });
+
+    editorSubmit.on('click', function() {
+        // build an object and send it using ajax
+
+        var data = {
+            id: $(this).data('id'),
+            name: nameEditor.val(),
+            score: scoreEditor.val(),
+            date_completed: dateEditor.val()
+        };
+
+        updateData(data);
+    });
+}
+
+function showEditor(id,name, score, date){
+    editorSubmit.attr('data-id', id);
+    nameEditor.val(name);
+    scoreEditor.val(score);
+    dateEditor.val(date);
+    editPanel.slideDown().delay().addClass('change');
+}
+
+function clearEditor(){
+    editorSubmit.attr('data-id', '');
+    nameEditor.val('');
+    scoreEditor.val('');
+    dateEditor.val('');
+    editPanel.slideUp().delay().removeClass('change');
 }
